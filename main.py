5f82d99b1e15cec8ca5fea2d1778a6251ee10a23
Icaro Alves <33491428+Roxicaro@users.noreply.github.com>
2025-04-04 17:58:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,21 @@ smap=se.Submap(map, 0, 0)
 block=se.Object("#")
 ground=se.Square("#", map.width, 5)
 player=se.Object(f'O')
-frame = se.Frame(height-1, width-1, 
+frame = se.Frame(height-1, width-5, 
                  corner_chars=["+", "+", "+", "+"], 
                  horizontal_chars=["-", "-"], 
                  vertical_chars=["|", "|"], state="solid")
 h=se.Text("00 00")
 
 player.add(map, round(smap.width/2), round(map.height/2))
-frame.add(map, 0,0)
+frame.add(map, 1,0)
 
 
-smap.remap()
+#smap.remap()
 smap.show(init=True)
-
-smap.remap()
-smap.show()
 smap.set(smap.x+1, smap.y)
+
+while True:
+    player.set()
+    smap.remap()
+    smap.show()
