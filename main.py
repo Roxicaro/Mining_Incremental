01940f093c4b311fd4cc8dca5f43f76a669584d2
Icaro Alves <33491428+Roxicaro@users.noreply.github.com>
2025-04-04 17:43:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,58 +6,25 @@ width, height = os.get_terminal_size()
 t=ev=v=0
 g=0.015
 
-map=se.Map(height-1, 1000, " ")
+map=se.Map(height, 1000, " ")
 smap=se.Submap(map, 0, 0)
 
 block=se.Object("#")
-panel=se.Square("#", 10, 1)
 ground=se.Square("#", map.width, 5)
-player=se.Object("t")
+player=se.Object(f'O')
+frame = se.Frame(height-1, width-1, 
+                 corner_chars=["+", "+", "+", "+"], 
+                 horizontal_chars=["-", "-"], 
+                 vertical_chars=["|", "|"], state="solid")
 h=se.Text("00 00")
 
-block.add(map, 200, map.height-6)
-panel.add(map, 100, map.height-10)
-ground.add(map, 0, map.height-5)
 player.add(map, round(smap.width/2), round(map.height/2))
-h.add(smap, 0, 0)
+frame.add(map, 0,0)
 
-def on_press(key):
-    global ev
-    ev=str(key)
-
-
-from pynput.keyboard import Key, Listener
-def recogniser():
-    global ev
-    while True:
-        with Listener(on_press=on_press) as listener:
-            listener.join()
-
-recognising=threading.Thread(target=recogniser)
-recognising.daemon=True
-recognising.start()
 
 smap.remap()
 smap.show(init=True)
-time.sleep(0.5)
-while True:
-    for ob in map.obs:
-        if player.y+1 == ob.y and player.x == ob.x:
-            t=0
-            v=0
-    if ev == "Key.enter":
-        v=-0.25
-        ev=0
-    player.set(player.x+1, player.y)
-    if player.set(player.x, round(player.y-(v*(v/g)-1/2*g*(v/g)**2)-v*t+1/2*g*t**2)) != 0 and t != 0:
-        player.set(player.x, player.y+1)
-    t+=1
-    exec("point_%s=se.Object('*', 'float')"%t)
-    exec("point_%s.add(map, player.x, player.y-1)"%t)
-    if player.x < smap.x-1:
-        exit()
-    h.rechar((2-len(str(player.y)))*" "+str(player.y)+" "+str(map.height))
-    time.sleep(0.05)
-    smap.remap()
-    smap.show()
-    smap.set(smap.x+1, smap.y)
+
+smap.remap()
+smap.show()
+smap.set(smap.x+1, smap.y)
