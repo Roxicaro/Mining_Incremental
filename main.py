88af8473dfbe316a9ea23c6a282ea0184af52b79
Icaro Alves <fly-too-high@hotmail.com>
2025-04-04 21:43:46 -0300

    no name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import scrap_engine as se
 import time, os, threading, sys
+import random
 
 os.system("")
 width, height = os.get_terminal_size()
@@ -16,17 +17,41 @@ frame = se.Frame(height-1, width-5,
                  corner_chars=["+", "+", "+", "+"], 
                  horizontal_chars=["-", "-"], 
                  vertical_chars=["|", "|"], state="solid")
-h=se.Text("00 00")
+h=se.Text(f'Width: {width}\nHeight: {height}', float)
 
 player.add(map, round(smap.width/2), round(map.height/2))
 frame.add(map, 1,0)
+h.add(map,10,3)
+
 
 
 #smap.remap()
 smap.show(init=True)
 smap.set(smap.x+1, smap.y)
 
+# Initialize direction variable
+direction = 1  # X direction changer
+y_change = 1
+
 while True:
-    player.set()
+    # Check if the player touches the frame side boundaries
+    if player.x >= frame.width -1:  # Right boundary
+        direction = -1  # Move left
+    elif player.x <= 2:  # Left boundary
+        direction = 1  # Move right
+
+    # Check if the player touches the frame upper/lower boundaries
+    if player.y <= 2:  # Right boundary
+        y_change = 1  # Move left
+    elif player.y >= frame.height -3:  # Left boundary
+        y_change = -1  # Move right
+    
+    # Move the player in the current direction
+    player.set(player.x+1 * direction, player.y+1 * y_change)
+
+    # Update and display the map
     smap.remap()
     smap.show()
+
+    # Add a small delay to control the speed of movement
+    time.sleep(0.03)
