2e2fe174aaacf9fcbd1fb8e56b83dab3c252dce3
Icaro Alves <fly-too-high@hotmail.com>
2025-04-06 12:03:24 -0300

    06/04/2025 12:00:00 PM

--- main.py
+++ main.py
@@ -12,12 +12,20 @@ g=0.015
 map=se.Map(height, 1000, " ")
 smap=se.Submap(map, 0, 0)
 
+# Create a frame with the specified height and width
 frame = se.Frame(height-2, width-1, 
                  corner_chars=["+", "+", "+", "+"], 
                  horizontal_chars=["-", "-"], 
                  vertical_chars=["|", "|"], state="solid")
-text=se.Text(f'Width: {width}\nHeight: {height}', float)
+frame.add(map, 1,0)
 
+
+#Rersources
+iron = -1
+
+# Create text
+text=se.Text(f'Iron: {iron}', float)
+text.add(map,3,1)
 
 # Player design data
 from player_design import PLAYER_DESIGN  # Import player design data
@@ -26,11 +34,8 @@ def create_player(map, start_x=5, start_y=5):
     player_parts = []
     for char, rel_x, rel_y in PLAYER_DESIGN:
         obj = se.Object(char).add(map, start_x + rel_x, start_y + rel_y)
-    player_parts.append(obj)
+        player_parts.append(obj)
     return player_parts
-
-# Create the player at the specified position
-create_player(map, 5, 5)  # Creates entire drill at (5,5)
 
 # Rock design data
 from rock_design import ROCK_DESIGN  # Import rock design data
@@ -43,11 +48,11 @@ def create_rock(map, start_x=25, start_y=5):
             rock_parts.append(obj)
     return rock_parts
 
-# Usage:
-boulder = create_rock(map, 25, 3)  # Creates rock at (10,3)
+# Create the player at the specified position
+player_parts = create_player(map, frame.width-16, frame.height-4)  # Creates entire drill at (5,5)
 
-frame.add(map, 1,0)
-text.add(map,10,3)
+# Place the rock at the specified position:
+rock = create_rock(map, frame.width-10, frame.height-5)  # Creates rock at (10,3)
 
 smap.show(init=True)
 smap.set(smap.x+1, smap.y)
@@ -56,25 +61,18 @@ smap.set(smap.x+1, smap.y)
 direction = 1  # X direction changer
 y_change = 1
 
-while True:
-    '''# Check if the player touches the frame side boundaries
-    if player.x >= frame.width -1:  # Right boundary
-        direction = -1  # Move left
-    elif player.x <= 2:  # Left boundary
-        direction = 1  # Move right
 
-    # Check if the player touches the frame upper/lower boundaries
-    if player.y <= 2:  # Right boundary
-        y_change = 1  # Move left
-    elif player.y >= frame.height -3:  # Left boundary
-        y_change = -1  # Move right
-    
-    # Move the player in the current direction
-    player.set(player.x+1 * direction, player.y+1 * y_change)'''
+#Resource IRON updater and drill animation
+iron_cd = 1
+drill_state = '►'
+while True:
+    iron += 1  # Increment iron count
+    text.rechar(f'Iron: {iron}')  # Update the text object with the new iron count
 
     # Update and display the map
     smap.remap()
     smap.show()
-
     # Add a small delay to control the speed of movement
-    time.sleep(0.03)
+    time.sleep(iron_cd)
+
+
